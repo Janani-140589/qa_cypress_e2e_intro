dd88fa8dbf330c51ec2eeea3e87580a7e034d649
Janani <jananiraja1405@gmail.com>
2025-04-28 12:43:58 +1200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,18 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    const userName = 'jananirj';
+    cy.visit('/');
 
+    // click sign in button
+    cy.get('a[href = "/user/login"]').should('contain.text', 'Sign in').click();
+
+    // Type email and password and click sign in
+    cy.get('input[placeholder="Email"]').type('jananiraja1405@gmail.com');
+    cy.get('input[placeholder="Password"]').type('India@123');
+    cy.get('button[type="submit"]').contains('Sign in').click();
+
+    // assert username after sign in
+    cy.get(`a[href="/profile/${userName}"]`).should('exist');
   });
 });
